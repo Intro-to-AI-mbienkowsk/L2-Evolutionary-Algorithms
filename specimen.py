44b1c7de5f0d64552540d21ce5553edc6f237223
maksym <01178511@pw.edu.pl>
2023-10-25 14:46:29 +0200

    class skeleton for specimen

---
+++ specimen.py
@@ -0,0 +1,19 @@
+import numpy as np
+from abc import ABC, abstractmethod, abstractproperty
+
+
+class Specimen(ABC):
+    def __init__(self, value: np.ndarray):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @abstractmethod
+    def mutate(self, mutation_params):
+        ...
+
+    @abstractmethod
+    def crossbreed(self, other, crossbreed_params):
+        ...
