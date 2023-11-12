d234b890757ebfb6991ff8b133e3dd57c71db5fb
maksym <01178511@pw.edu.pl>
2023-11-12 11:41:20 +0100

    returning average values from evolve

--- evolution.py
+++ evolution.py
@@ -5,13 +5,16 @@ class Evolution:
     def __init__(self, population: Population, epochs: int):
         self.population = population
         self.best_specimens = []
+        self.average_specimens = []
         self.epochs = epochs
 
     def perform_epoch(self):
         self.population.reproduce()
         self.population.mutate()
-        return self.population.find_best_specimen()
+        return self.population.find_best_specimen(), self.population.average_goal_function_value()
 
     def evolve(self):
         for _ in range(self.epochs):
-            self.best_specimens.append(self.perform_epoch())
+            best, avg = self.perform_epoch()
+            self.best_specimens.append(best)
+            self.average_specimens.append(avg)
