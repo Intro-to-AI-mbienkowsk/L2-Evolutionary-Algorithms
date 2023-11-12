768bff328e82aa023f95f0e55382bb8410549f18
maksym <01178511@pw.edu.pl>
2023-11-12 14:06:21 +0100

    main function implementation + add legend to plot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from evolution import Evolution
-from specimen import TSPSpecimen
 from population import TSPPopulation
+from constants import DEFAULT_POPULATION, DEFAULT_MUTATION_STRENGTH, DEFAULT_EPOCHS
+from argparse import ArgumentParser
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -17,7 +18,26 @@ def plot_data(best_vals, average_vals=None):
     else:
         ax.set_title('Best value of the goal function in each epoch')
     plt.grid(True)
+    plt.legend()
     plt.show()
 
 
+def main():
+    parser = ArgumentParser(description='Find optimal route for TSP using an evolutional algorithm.')
+    parser.add_argument('-p', type=int, default=DEFAULT_POPULATION,
+                        help='size of the population, int')
+    parser.add_argument('-s', type=float, default=DEFAULT_MUTATION_STRENGTH,
+                        help='mutation strength between 0 and 1, float')
+    parser.add_argument('-e', type=int, default=DEFAULT_EPOCHS,
+                        help='number of epochs to perform, int')
+    # todo: reproduction method
+    # todo: make average_specimens optional
+    args = parser.parse_args()
+    population = TSPPopulation(mutation_strength=args.s, num_of_specimens=args.p)
+    evolution = Evolution(population, args.e)
+    evolution.evolve()
+    plot_data(evolution.best_specimens, evolution.average_specimens)
 
+
+if __name__ == '__main__':
+    main()
